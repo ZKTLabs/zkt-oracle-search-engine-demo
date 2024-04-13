c75fa5d832de4d7c0b561eca1ee17ac6984a2847
zouhang <1102639563@qq.com>
2024-04-13 15:20:46 +0800

    chore: update logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,12 @@
 import { formatAmount } from '@did-network/dapp-sdk'
 import BigNumber from 'bignumber.js'
-import { parseEther } from 'viem'
-import {
-  useAccount,
-  useBalance,
-  useBlockNumber,
-  useReadContract,
-  useWaitForTransactionReceipt,
-  useWriteContract,
-} from 'wagmi'
+import { parseEther, zeroAddress } from 'viem'
+import { useAccount, useBalance, useBlockNumber, useWaitForTransactionReceipt, useWriteContract } from 'wagmi'
 
 import { Header } from '@/components/layout/Header'
-import { useTokenContract } from '@/hooks'
 
 import abi from '../abi/PoolSwapTest.json'
 
-const MAX_ALLOWANCE = BigInt(BigNumber(2).pow(256).minus(1).toString())
-
-const MockTokenA = '0x520A3474beAaE4AC406242aa74eF6D052dE8aaED'
 const MockTokenB = '0x6BCCF17873Fe200962451E6824090b847DB1ACEb'
 const PoolSwapTest = '0x92d3117268Bd580a748acbEE73162834443a3A17'
 const ZKTUniswapV4Hook = '0x020951DEDa6928a0Eb297ed5e6a4132A01d800DD'
@@ -29,51 +18,27 @@ const Home = () => {
   })
   const { data: hash, writeContractAsync, isPending, error } = useWriteContract()
 
-  const [tokenPair, setTokenPair] = useState(['usdt', 'usdc'])
+  const [tokenPair, setTokenPair] = useState(['eth', 'usdt'])
   const reverseTokenPair = useCallback(() => {
     setTokenPair([tokenPair[1], tokenPair[0]])
   }, [tokenPair])
 
   const [inAmount, setInAmount] = useState('')
-  const outAmount = inAmount
-  const usdcContract = useTokenContract(MockTokenA)
-  const usdtContract = useTokenContract(MockTokenB)
+  const outAmount = useMemo(
+    () =>
+      BigNumber(inAmount || 0)
+        .times(25000)
+        .toString(),
+    [inAmount]
+  )
 
-  const { data: usdcBalance, refetch: usdcBalanceRefetch } = useBalance({
+  const { data: ethBalance, refetch: usdcBalanceRefetch } = useBalance({
     address,
-    token: MockTokenA,
   })
   const { data: usdtBalance, refetch: usdtBalanceRefetch } = useBalance({
     address,
     token: MockTokenB,
   })
-  const { data: usdtAllowanceData, refetch: usdtRefetch } = useReadContract({
-    ...usdtContract,
-    functionName: 'allowance',
-    args: [address!, ZKTUniswapV4Hook!],
-  })
-  const { data: usdcAllowanceData, refetch: usdcRefetch } = useReadContract({
-    ...usdcContract,
-    functionName: 'allowance',
-    args: [address!, ZKTUniswapV4Hook!],
-  })
-  const allowance = useMemo(
-    () => (tokenPair[0] === 'usdt' ? usdtAllowanceData : usdcAllowanceData) ?? 0n,
-    [tokenPair, usdtAllowanceData, usdcAllowanceData]
-  )
-  const isApproved = useMemo(() => typeof allowance !== 'undefined' && allowance > 0, [allowance])
-
-  const approveMu = useWriteContract()
-  const approveHandler = useCallback(
-    async (maxAmount = MAX_ALLOWANCE) => {
-      await approveMu.writeContractAsync({
-        ...(tokenPair[0] === 'usdt' ? usdtContract : usdcContract),
-        functionName: 'approve',
-        args: [ZKTUniswapV4Hook, maxAmount],
-      })
-    },
-    [approveMu, tokenPair, usdcContract, usdtContract]
-  )
 
   const onSwap = useCallback(async () => {
     await writeContractAsync({
@@ -81,14 +46,14 @@ const Home = () => {
       abi,
       functionName: 'swap',
       args: [
-        [MockTokenA, MockTokenB, 3000, 60, ZKTUniswapV4Hook],
-        [true, parseEther(inAmount), '7922816251426433759354395033'],
+        [zeroAddress, MockTokenB, 50000, 60, ZKTUniswapV4Hook],
+        [true, parseEther('200'), '17715778550986650090239814974724'],
         [true, true, false],
         '0x',
       ],
-      value: 0n,
+      value: parseEther('0.01'),
     })
-  }, [inAmount, writeContractAsync])
+  }, [writeContractAsync])
 
   const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({
     hash,
@@ -97,18 +62,15 @@ const Home = () => {
 
   useEffect(() => {
     console.log('[blockNumber]', blockNumber)
-    usdcRefetch()
-    usdtRefetch()
     usdcBalanceRefetch()
     usdtBalanceRefetch()
-  }, [blockNumber, usdcBalanceRefetch, usdcRefetch, usdtBalanceRefetch, usdtRefetch])
+  }, [blockNumber, usdcBalanceRefetch, usdtBalanceRefetch])
 
   const clickHandler = useCallback(async () => {
-    if (!isApproved || allowance < parseEther(inAmount)) {
-      await approveHandler(isApproved ? allowance + parseEther(inAmount) : MAX_ALLOWANCE)
-    }
     await onSwap()
-  }, [allowance, approveHandler, inAmount, isApproved, onSwap])
+  }, [onSwap])
+
+  const btnDisabled = useMemo(() => !inAmount || isConfirming || isPending, [inAmount, isConfirming, isPending])
 
   return (
     <>
@@ -129,26 +91,27 @@ const Home = () => {
                 onChange={(e) => setInAmount(e.target.value)}
               />
               <div className="p-1 px-1.5 shadow-sm flex-center gap-1 bg-white rounded-full cursor-pointer">
-                <span className="shrink-0 w-6 h-6 i-cryptocurrency-color:usdc"></span>
+                <span className="shrink-0 w-6 h-6 i-cryptocurrency-color:eth"></span>
                 <span className="font-600 text-xl uppercase">{tokenPair[0]}</span>
                 <span className="w-5 h-5 i-lucide-chevron-down"></span>
               </div>
             </div>
             <div className="flex items-center justify-between text-sm text-gray-5 font-500">
-              <span>${formatAmount(inAmount)}</span>
+              <span>${formatAmount(outAmount)}</span>
               <span>
                 <span>Balance: </span>
-                <span className="">{formatAmount(usdcBalance?.value, usdcBalance?.decimals)}</span>
+                <span className="">{formatAmount(ethBalance?.value, ethBalance?.decimals)}</span>
               </span>
             </div>
           </div>
 
-          <span
+          <button
             className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-10 h-10 flex-col-center border-4 box-border border-solid border-white rounded-lg bg-#f9f9f9 cursor-pointer"
             onClick={reverseTokenPair}
+            disabled
           >
             <span className="i-lucide:arrow-down"></span>
-          </span>
+          </button>
 
           <div className="px-4 py-4 space-y-2 rounded-lg border-transparent [&:has(input:focus)]:border-border border-1px border-solid bg-#f9f9f9">
             <div className="text-sm text-gray-5 font-500">You receive</div>
@@ -167,7 +130,7 @@ const Home = () => {
               </div>
             </div>
             <div className="flex items-center justify-between text-sm text-gray-5 font-500">
-              <span>${formatAmount(outAmount)}</span>
+              <span>${formatAmount(outAmount, 0, 2)}</span>
               <span>
                 <span>Balance: </span>
                 <span className="">{formatAmount(usdtBalance?.value, usdtBalance?.decimals)}</span>
@@ -178,13 +141,13 @@ const Home = () => {
 
         <button
           onClick={clickHandler}
-          disabled={!inAmount || approveMu.isPending || isConfirming || isPending}
+          disabled={btnDisabled}
           className="mt-1 w-full h-14 flex-center gap-1 rounded-lg transition-all text-5 font-600 text-white bg-primary active:bg-primary/80 disabled:bg-primary/50"
         >
-          {(approveMu.isPending || isPending || isConfirming) && (
+          {(isPending || isConfirming) && (
             <span className="i-lucide:loader-circle w-5 h-5 text-white animate-spin"></span>
           )}
-          {isApproved ? 'Swap' : 'Approve'}
+          Swap
         </button>
       </div>
     </>
